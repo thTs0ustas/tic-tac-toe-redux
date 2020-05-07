85da4190f216c3b96718dac10ba18bc9d6a77dfa
thTs0ustas <60882323+thTs0ustas@users.noreply.github.com>
2020-05-07 19:35:49 +0300

    cleanup

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -52,33 +52,3 @@ export const reducer = (state = init, action) => {
       return state;
   }
 };
-
-/* 
-export const reducer = (state = init, action) => {
-  let { value, history } = state.payload;
-  let newValue = value.slice();
-  switch (action.type) {
-    case X_PLAYS:
-      newValue[action.payload.index] = action.payload.value;
-      return Object.assign({}, state, {
-        payload: {
-          history: [newValue],
-          value: newValue,
-          xIsNext: !state.payload.xIsNext,
-        },
-      });
-    case O_PLAYS:
-      newValue[action.payload.index] = action.payload.value;
-      return Object.assign({}, state, {
-        payload: {
-          history: [newValue],
-          value: newValue,
-          xIsNext: !state.payload.xIsNext,
-        },
-      });
-    case NAME:
-      return [...state.playerNames, { playerNames: action.payload }];
-    default:
-      return state;
-  }
-}; */
